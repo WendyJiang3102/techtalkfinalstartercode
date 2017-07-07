bf2a6d1187cd00a382ed075b79427c8ed0330d15
Wendy Jiang <wjiang.student@hsfi.us>
2017-07-07 19:24:04 +0000

    need to fix script

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // JavaScript File
 // JavaScript File
 $( document ).ready(function() {
-
+$("button").click(function(){
+    var inputValue= $("#submit").val();
+    
+    if(inputValue ==="ScriptEd"){
+        $("#result").html("Horray!");
+        for(var count = 0; count <5; count++){
+             $("body").append("<img src='https://media.giphy.com/media/hEIuLmpW9DmGA/giphy.gif'>");
+    }else if(inputValue ==="scripted"){
+        $("#result").html("Horray!");
+    }else{
+        $("#result").html("Try Again!");
+    }
+});
 
 });
