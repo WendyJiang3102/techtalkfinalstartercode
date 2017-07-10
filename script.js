40e1efefad524b7080eb22cfd185be5c92c3cb9f
Wendy Jiang <wjiang.student@hsfi.us>
2017-07-10 15:58:47 +0000

    fixed button

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 // JavaScript File
 // JavaScript File
 $( document ).ready(function() {
-$("button").click(function(){
+$("#value").click(function(){
     var inputValue= $("#submit").val();
     
     if(inputValue ==="ScriptEd"){
         $("#result").html("Horray!");
-        for(var count = 0; count <5; count++){
-             $("body").append("<img src='https://media.giphy.com/media/hEIuLmpW9DmGA/giphy.gif'>");
-    }else if(inputValue ==="scripted"){
+            for(var count = 0; count <5; count++){
+            $("#result").html('<img src="https://media.giphy.com/media/hEIuLmpW9DmGA/giphy.gif">');}
+    }else if(inputValue === "scripted"){
         $("#result").html("Horray!");
+    
     }else{
         $("#result").html("Try Again!");
     }
-});
-
+    $("input").val("");
+    });
 });
